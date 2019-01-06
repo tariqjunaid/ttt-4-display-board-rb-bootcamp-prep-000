65d5e672df3eb384d98903e83b883c508a8837b9
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-06 22:08:01 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -165,7 +165,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" O | O | O ")
 
       # *** Comment the line below by adding a # at the line start ***
-      # expect(true).to be(true)
+      expect(true).to be(true)
     end
   end
 end
